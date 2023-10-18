303cc5284da642e34cea15d1cf5286d24e9efbc4
Fady Raafat <fady.raafat.ramzy@gmail.com>
2023-10-18 17:32:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notification/ui/presentation/main_screen/NotificationService.kt
@@ -0,0 +1,41 @@
+package com.example.notification.ui.presentation.main_screen
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.example.notification.MainActivity
+import com.example.notification.R
+
+class NotificationService(
+    val msg : String ,
+    val title : String ,
+    val context : Context
+) {
+    val channelId = "My_Channel"
+    val channelName = "My Channel Name"
+    val notificationManager = context.applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    lateinit var  notificationChannel : NotificationChannel
+    lateinit var notificationBuilder : NotificationCompat.Builder
+
+    fun showNotification(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            notificationChannel = NotificationChannel(channelId,channelName,NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        val intent = Intent(context,MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(context,0,intent,PendingIntent.FLAG_IMMUTABLE)
+        notificationBuilder = NotificationCompat.Builder(context , channelId)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .addAction(R.drawable.ic_launcher_background,"Open Notification" ,pendingIntent)
+            .setContentTitle(title)
+            .setContentText(msg)
+            .setAutoCancel(true)
+        notificationManager.notify(100,notificationBuilder.build())
+
+    }
+}
